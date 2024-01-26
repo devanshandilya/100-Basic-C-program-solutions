628f0249a2f00922d0fde32d98a948d05116ae6d
Devansh Shandilya <122812994+devanshandilya@users.noreply.github.com>
2024-01-26 19:43:41 +0530

    Update vectormatrix.cpp

--- vectormatrix.cpp
+++ vectormatrix.cpp
@@ -1,30 +1,35 @@
+//print a matrix using 2-D vectors
+//print a matrix using function
+//2-D vectors basics
 #include<iostream>
-#include <vector>
+#include <vector> //using vector library
 using namespace std;
-//2-D vectors
-//print matrix using 2D vectors
-void fun(int m, int n, vector<vector<int> > mat)
+
+void fun(int m, int n, vector<vector<int> > mat) //declaring funtion with 2-D vector integer declared as mat
 {
        for(int i=0;i<m;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<n;j++) //2-D loop to get integers line by line i.e row and column like a matrix
         {
-            cout<<mat[i][j]<<" ";
+            cout<<mat[i][j]<<" "; //printing the matrix separated by space with i and j being the rows and columns in the loop
         }
     }
 }
 int main()
 {
     int n,m;
+    cout<<"Enter the number of rows: ";
     cin>>m;
+    cout<<"Enter the number of columns: ";
     cin>>n;
-    vector<vector<int> > mat(m,vector<int> (n));
+    vector<vector<int> > mat(m,vector<int> (n)); //declaring a 2-D vector called mat
+    cout<<"Enter elements in matrix: "<<endl; //endl to end line so as to input elements in a pattern from the next line
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
         {
-            cin>>mat[i][j];
+            cin>>mat[i][j];  //enter matrix following the rows and columns or enter the matrix in one line, output will be in matrix form either way
         }
     }
-    fun(n,m,mat);
+    cout<<"matrix is: "<<fun(n,m,mat);
 }
